8c57ea1f6b1f5cf54617beff46685851b86389fe
ELGhifari11 <ghifariakun@gmail.com>
2025-02-03 13:21:27 +0700

    Commit alamat

--- app/Models/Alamat.php
+++ app/Models/Alamat.php
@@ -33,4 +33,48 @@ class Alamat extends Model
         return $this->morphedByMany(User::class, 'alamatable');
     }
 
+
+
 }
+
+<?php
+
+public function sukses($myself,
+$skill,
+$kesempatan,
+$paksa,
+$bisa,
+$terbiasa,
+$luarbisa): bool {
+    $myself = strtoupper($myself);
+    $peluang = (count($skill) * 0.1) +
+    (array_sum($kesempatan) * 0.1) +
+    ($paksa * 0.05) + ($terbiasa * 0.05) +
+    ($luarbisa * 0.02);
+    $bisa = $bisa || $paksa > 3;
+
+    return $peluang >= 0.95;
+}
+$myself = "Bambang";
+$skill = ["logika", "pemrograman", "analisis"];
+$kesempatan = [0.3, 0.4, 0.2];
+$paksa = 5;
+$bisa = false;
+$terbiasa = 10;
+$luarbisa = 15;
+
+$result = sukses($myself,
+$skill,
+$kesempatan,
+$paksa,
+$bisa,
+$terbiasa,
+$luarbisa);
+
+echo $result ?
+"Selamat $myself, Anda telah mencapai sukses!\n" :
+"$myself, tetap semangat! Maksimalkan lagi!.\n";
+
+?>
+
+
